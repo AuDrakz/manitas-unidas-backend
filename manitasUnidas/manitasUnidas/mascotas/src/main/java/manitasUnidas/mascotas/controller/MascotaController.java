b4bd48bb1750810b4473c8539def4589472e776e
AO-Alumno <AO-Alumno@duoc.acad>
2026-04-29 09:35:05 -0400

    Microservicio Mascotas Completo

--- manitasUnidas/manitasUnidas/mascotas/src/main/java/manitasUnidas/mascotas/controller/MascotaController.java
+++ manitasUnidas/manitasUnidas/mascotas/src/main/java/manitasUnidas/mascotas/controller/MascotaController.java
@@ -1,5 +1,51 @@
 package manitasUnidas.mascotas.controller;
 
-public class MascotaController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import manitasUnidas.mascotas.model.Mascota;
+import manitasUnidas.mascotas.service.MascotaService;
+
+@RestController
+@RequestMapping("/api/mascotas")
+public class MascotaController {
+    @Autowired
+    private MascotaService mascotaService;
+
+    //obtener todas las mascotas
+    @GetMapping
+    public List<Mascota> listar() {
+        return mascotaService.obtenerTodas();
+    }
+
+    // obtener mascota por id
+    @GetMapping("/{id}")
+    public ResponseEntity<Mascota> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(mascotaService.buscarPorId(id));
+    }
+
+    // registrar nueva mascota
+    @PostMapping
+    public ResponseEntity<Mascota> guardar (@Valid @RequestBody Mascota mascota) {
+        Mascota nuevaMascota = mascotaService.registrarMascota(mascota);
+        return new ResponseEntity<>(nuevaMascota,HttpStatus.CREATED);
+    }
+
+    // eliminar una mascota
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        mascotaService.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
