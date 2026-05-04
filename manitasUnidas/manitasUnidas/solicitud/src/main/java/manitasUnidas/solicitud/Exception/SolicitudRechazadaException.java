637851d3a81121a5d38a1a6f8391a1d4156bdeaf
AO-Alumno <AO-Alumno@duoc.acad>
2026-05-04 10:00:05 -0400

    feat: se actualizo microservicios

---
+++ manitasUnidas/manitasUnidas/solicitud/src/main/java/manitasUnidas/solicitud/Exception/SolicitudRechazadaException.java
@@ -0,0 +1,8 @@
+package manitasUnidas.solicitud.Exception;
+
+public class SolicitudRechazadaException extends RuntimeException{
+    public SolicitudRechazadaException(String mensaje) {
+        super(mensaje);
+    }
+
+}
