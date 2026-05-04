77b6aa8004af8e0c5fb2e9015172f963583b80f0
AO-Alumno <AO-Alumno@duoc.acad>
2026-05-04 10:28:47 -0400

    apiGateway completo

--- manitasUnidas/manitasUnidas/apiGateway/src/main/java/manitasUnidas/apiGateway/ApiGatewayApplication.java
+++ manitasUnidas/manitasUnidas/apiGateway/src/main/java/manitasUnidas/apiGateway/ApiGatewayApplication.java
@@ -2,12 +2,13 @@ package manitasUnidas.apiGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient; // <--- Agregar esto
 
 @SpringBootApplication
+@EnableDiscoveryClient // <--- Esta anotación es la "antena"
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
