ff177249529e0a2a1f68180f09817b86c92e5cba
AO-Alumno <AO-Alumno@duoc.acad>
2026-05-04 10:17:15 -0400

    Eureka Completo y update propierties Ficha(Completo)

--- manitasUnidas/manitasUnidas/eureka/src/main/java/manitasUnidas/eureka/EurekaApplication.java
+++ manitasUnidas/manitasUnidas/eureka/src/main/java/manitasUnidas/eureka/EurekaApplication.java
@@ -2,12 +2,12 @@ package manitasUnidas.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer; // <-- IMPORTANTE
 
 @SpringBootApplication
+@EnableEurekaServer // <--- Esta es la que hace la magia
 public class EurekaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
 }
