55d99048653caebbe4cd0458f310eeafe0ca2917
AO-Alumno <AO-Alumno@duoc.acad>
2026-05-04 09:54:46 -0400

    Ficha Actualizada con exception,propierties y aplication

--- manitasUnidas/manitasUnidas/fichaVet/src/main/java/manitasUnidas/fichaVet/FichaVetApplication.java
+++ manitasUnidas/manitasUnidas/fichaVet/src/main/java/manitasUnidas/fichaVet/FichaVetApplication.java
@@ -2,12 +2,12 @@ package manitasUnidas.fichaVet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FichaVetApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FichaVetApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FichaVetApplication.class, args);
+    }
 }
